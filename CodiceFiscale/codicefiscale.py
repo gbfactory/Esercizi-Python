cefaef6aa1d7f0fb9dcfe093e5b663c8b8fb793a
gb-factory <37420179+gb-factory@users.noreply.github.com>
2020-02-24 23:52:26 +0100

    Recode completo

--- CodiceFiscale/codicefiscale.py
+++ CodiceFiscale/codicefiscale.py
@@ -1,259 +1,125 @@
 # Codice Fiscale
 # 18 01 2020
 
-cogn = input("Inserisci il cognome: ")
-noms = input("Inserisci il nome: ")
-sess = input("Inserisci il sesso: ")
-dataGiorno = int(input("Inserisci il giorno di nascita: "))
-dataMese = int(input("Inserisci il mese di nascita: "))
-dataAnno = int(input("Inserisci l'anno di nascita: "))
-luogo = input("Inserisci il luogo di nascita: ")
+cognome = (input("Inserire il cognome: ")).replace(" ", "")
+nome = (input("Inserire il nome: ")).replace(" ", "")
+sesso = (input("inserire sesso(F o M): ")).upper()
+data = input("inserire data di nascita (gg/mm/anno): ").split('/')
+comune = input("inserire comune di nacita: ")
 
-codicefiscale = ""
+vocali = "aeiouAEIOU"
 
-# CALCOLO COGNOME
-cognome = cogn.upper()
-cCons = ""
-cVocs = ""
+mesi = {'01': 'A', '02': 'B', '03': 'C', '04': 'D',
+        '05': 'E', '06': 'H', '07': 'L', '08': 'M',
+        '09': 'P', '10': 'R', '11': 'S', '12': 'T'}
 
-i = 0
-for i in cognome:
-    c = i
-    if c == 'A' or c == 'E' or c == 'I' or c == 'O' or c == 'U':
-        if len(cVocs) < 3:
-            cVocs = cVocs + c
+pari = {'0': 0, '1': 1, '2': 2, '3': 3, '4': 4, '5': 5,
+        '6': 6, '7': 7, '8': 8, '9': 9, 'A': 0, 'B': 1,
+        'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7,
+        'I': 8, 'J': 9, 'K': 10, 'L': 11, 'M': 12, 'N': 13,
+        'O': 14, 'P': 15, 'Q': 16, 'R': 17, 'S': 18, 'T': 19,
+        'U': 20, 'V': 21, 'W': 22, 'X': 23, 'Y': 24, 'Z': 25}
+
+
+dispari = {'0': 1, '1': 0, '2': 5, '3': 7, '4': 9, '5': 13,
+           '6': 15, '7': 17, '8': 19, '9': 21, 'A': 1, 'B': 0,
+           'C': 5, 'D': 7, 'E': 9, 'F': 13, 'G': 15, 'H': 17,
+           'I': 19, 'J': 21, 'K': 2, 'L': 4, 'M': 18, 'N': 20,
+           'O': 11, 'P': 3, 'Q': 6, 'R': 8, 'S': 12, 'T': 14,
+           'U': 16, 'V': 10, 'W': 22, 'X': 25, 'Y': 24, 'Z': 23}
+
+controllo = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F',
+             6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L',
+             12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R',
+             18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X',
+             24: 'Y', 25: 'Z'}
+
+def CalcCognome(cognome):
+    cons = []
+    voc = []
+    for x in cognome:
+        if x in vocali:
+            voc.append(x)
         else:
-            cCons = cCons + c
-            if len(cCons) == 3:
-                cVocs = ""
-                break
+            cons.append(x)
+    codCognome = "".join(cons + voc + ['x'] * 2)[0:3]
 
-if len(cCons) < 3:
-    if len(cCons) == 2:
-        cCons = cCons + cVocs[0]
-    if len(cCons) == 1:
-        if len(cCons) > 1:
-            cCons = cCons + cVocs[0] + cVocs[1]
+    #print(codCognome.upper())
+    return codCognome.upper()
+
+
+def CalcNome(nome):
+    cons = []
+    voc = []
+    for x in nome:
+        if x in vocali:
+            voc.append(x)
         else:
-            cCons = cCons + cVocs[0] + "X"
-    if len(cCons) == 0:
-        if len(cVocs) < 3:
-            cCons = cCons + cVocs + "X"
+            cons.append(x)
+    if len(cons) > 3:
+        cons[1:2] = []
+    codNome = "".join(cons + voc + ['x'] * 2)[0:3]
+
+    #print(codNome.upper())
+    return codNome.upper()
+
+
+def Data(data, sesso):
+    anno = data[2][2:]
+    mese = mesi[data[1]]
+    if sesso == "F":
+        giorno = data[0] + 40
+    elif sesso == "M":
+        giorno = data[0]
+
+    #print(anno)
+    #print(mese)
+    #print(giorno)
+    return anno + mese + giorno
+
+
+def CalcCodComune(comune):
+    com = comune.upper()
+
+    f = open("comuni.txt", "r")
+    for x in f:
+        sCom = x.split(" -- ")
+        if sCom[0] == com:
+            codCat = sCom[1]
+            break
+
+    #print(codCat)
+    return codCat.rstrip()
+
+#print(codice1)
+
+def CodiceControllo(codFisc):
+    i = 0
+    pariSomma = 0
+    dispariSomma = 0
+    p = []
+    d = []
+    while i < len(codFisc):
+        if i % 2 != 0:
+            #print(codFisc[i])
+            p.append(codFisc[i])
         else:
-            cCons = cVocs
+            #print(codFisc[i])
+            d.append(codFisc[i])
+        i += 1
 
-codicefiscale = codicefiscale + cCons
+    for x in p:
+        #print(x)
+        pariSomma += pari[x]
 
-# CALCOLO NOME
-nome = noms.upper()
+    for y in d:
+        #print(y)
+        dispariSomma += dispari[y]
 
-nCons = ""
-nVocs = ""
+    carattere = controllo[((pariSomma + dispariSomma) % 26)]
 
-i = 0
-for i in nome:
-    c = i
-    if c == 'A' or c == 'E' or c == 'I' or c == 'O' or c == 'U':
-        if len(nVocs) < 3:
-            nVocs = nVocs + c
-        else:
-            nCons = nCons + c
-            if len(nCons) == 4:
-                nVocs = ""
-                break
+    return carattere.upper()
 
-if len(nCons) < 3:
-    if len(nCons) == 2:
-        nCons = nVocs[0]
-    if len(nCons) == 1:
-        if len(nVocs) > 1:
-            nCons = nCons[0] + nVocs[1]
-        else:
-            nCons = nCons + nVocs[0] + 'X'
-    if len(nCons) == 0:
-        if len(nVocs) < 3:
-            nVocs = nVocs + 'X'
-        else:
-            nCons = nVocs
+codice1 = CalcCognome(cognome) + CalcNome(nome) + Data(data, sesso) + CalcCodComune(comune)
 
-if len(nCons) > 3:
-    temp = nCons
-    nCons = ""
-    nCons = nCons + temp[0] + temp[2] + temp[3]
-
-codicefiscale = codicefiscale + nCons
-
-# ANNO
-anno = dataAnno % 100
-
-if anno <= 9:
-    annoString = "0" + str(anno)
-else:
-    annoString = str(anno)
-
-codicefiscale = codicefiscale + annoString
-
-# MESE
-if dataMese == 1:
-    meseCode = "A"
-elif dataMese == 2:
-    meseCode = "B"
-elif dataMese == 3:
-    meseCode = "C"
-elif dataMese == 4:
-    meseCode = "D"
-elif dataMese == 5:
-    meseCode = "E"
-elif dataMese == 6:
-    meseCode = "H"
-elif dataMese == 7:
-    meseCode = "L"
-elif dataMese == 8:
-    meseCode = "M"
-elif dataMese == 9:
-    meseCode = "P"
-elif dataMese == 10:
-    meseCode = "R"
-elif dataMese == 11:
-    meseCode = "S"
-elif dataMese == 12:
-    meseCode = "T"
-
-codicefiscale = codicefiscale + meseCode
-
-# GIORNO
-sesso = sess.upper()
-if sesso == "F":
-    giornoCode = str(dataGiorno + 40)
-else:
-    giornoCode = str(dataGiorno)
-
-codicefiscale = codicefiscale + giornoCode
-
-# CODICE CATASTALE
-comune = luogo.upper()
-
-f = open("comuni.txt", "r")
-for x in f:
-    sCom = x.split(" -- ")
-    if sCom[0] == comune:
-        codCat = sCom[1]
-        break
-
-codicefiscale = codicefiscale + codCat
-
-# CARATTERE DI CONTROLLO
-
-# caratteri pari
-pari = ""
-for i in range(len(codicefiscale)):
-    if i % 2 == 0:
-        pari = pari + codicefiscale[i]
-
-# caratteri dispari
-dispari = ""
-for i in range(len(codicefiscale)):
-    if i % 2 == 0:
-        dispari = dispari + codicefiscale[i]
-
-# conversione char dispari
-rDispari = 0
-for i in dispari:
-    if i == 0 or i == 'A':
-        rDispari = rDispari + 1
-        break
-    elif i == 1 or i == 'B':
-        rDispari = rDispari + 0
-        break
-    elif i == 2 or i == 'C':
-        rDispari = rDispari + 5
-        break
-    elif i == 3 or i == 'D':
-        rDispari = rDispari + 7
-        break
-    elif i == 4 or i == 'E':
-        rDispari = rDispari + 9
-        break
-    elif i == 5 or i == 'F':
-        rDispari = rDispari + 13
-        break
-    elif i == 6 or i == 'G':
-        rDispari = rDispari + 15
-        break
-    elif i == 7 or i == 'H':
-        rDispari = rDispari + 17
-        break
-    elif i == 8 or i == 'I':
-        rDispari = rDispari + 19
-        break
-    elif i == 9 or i == 'J':
-        rDispari = rDispari + 21
-        break
-    elif i == 'K':
-        rDispari = rDispari + 2
-        break
-    elif i == 'L':
-        rDispari = rDispari + 4
-        break
-    elif i == 'M':
-        rDispari = rDispari + 18
-        break
-    elif i == 'N':
-        rDispari = rDispari + 20
-        break
-    elif i == 'O':
-        rDispari = rDispari + 11
-        break
-    elif i == 'P':
-        rDispari = rDispari + 3
-        break
-    elif i == 'Q':
-        rDispari = rDispari + 6
-        break
-    elif i == 'R':
-        rDispari = rDispari + 8
-        break
-    elif i == 'S':
-        rDispari = rDispari + 12
-        break
-    elif i == 'T':
-        rDispari = rDispari + 14
-        break
-    elif i == 'U':
-        rDispari = rDispari + 16
-        break
-    elif i == 'V':
-        rDispari = rDispari + 10
-        break
-    elif i == 'W':
-        rDispari = rDispari + 22
-        break
-    elif i == 'X':
-        rDispari = rDispari + 25
-        break
-    elif i == 'Y':
-        rDispari = rDispari + 24
-        break
-    elif i == 'Z':
-        rDispari = rDispari + 23
-        break
-
-# conversione valori pari
-rPari = 0
-
-for i in pari:
-    n = ord(i)
-
-    if i.isalpha():
-        rPari = rPari + n
-    else:
-        x = n - 65
-        rPari = rPari + x
-
-# somma
-somma = rDispari + rPari
-resto = int(somma % 26)
-restoConv = ord(resto) + 65
-codicefiscale = codicefiscale + restoConv
-
-print(codicefiscale)
+print(codice1 + CodiceControllo(codice1))
